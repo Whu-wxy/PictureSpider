6bba89f1243fb10ab575f0561ec75e6048108d1c
Whu-wxy <641645560@qq.com>
2020-03-31 22:18:39 +0800

    1

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 base_url = "https://morvanzhou.github.io/"
 #https://morvanzhou.github.io/
 #http://www.scmuseum.cn/list-1657.html
-max_page_count = None       # 最大爬取页数：数字或None(一直爬取)
+max_page_count = 10       # 最大爬取页数：数字或None(一直爬取)
