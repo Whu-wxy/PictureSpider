622ed657290cb0b70e3aeec3acdd28bac2d1f194
Whu-wxy <641645560@qq.com>
2020-04-01 23:30:25 +0800

    add url_keyword

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
-base_url = "https://morvanzhou.github.io/"
+# 开始爬取的网址
+base_url = "http://www.zhejiangmuseum.com/zjbwg/collection/zpmcollection.html"
+#http://www.zhejiangmuseum.com/zjbwg/collection/zpmcollection.html
+#https://www.zjmuex.com/Collection/List/TSGC?etype=&areac=2ebfaf78-7f8c-47e9-8862-37b57baf05ea&city=80811da7-9a34-4705-8681-f88dec3a3c8b&fclass=&unit=2&title=
+#https://www.deepin.org/
 #https://morvanzhou.github.io/
 #http://www.scmuseum.cn/list-1657.html
-max_page_count = 10       # 最大爬取页数：数字或None(一直爬取)
+
+# 最大爬取页数：数字或None(一直爬取)
+max_page_count = None       
+
+# 先用审查元素找到关键词，用url中的关键词筛选url,用|隔开，为空则选取全部url
+url_keyword = 'Collection|UploadFiles'
+
